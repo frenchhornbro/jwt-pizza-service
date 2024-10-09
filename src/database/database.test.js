0ad8b2bf68f7c0662e4917ed31f53292fd92d22a
Hyrum Durfee <htdurfee@gmail.com>
2024-10-09 13:04:13 -0600

    add(test) createFranchise with no admins

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -9,3 +9,14 @@ test('updateUser', async () => {
     const update = await DB.updateUser(userID, null, null);
     expect(update.message).toBe('nothing to change');
 });
+
+test('createFranchiseNoAdmin', async () => {
+    const franchise = {name: randomName(), admins: [{email: randomName()}]};
+    let temp;
+    try {
+        temp = await DB.createFranchise(franchise);
+    }
+    catch {
+        expect(temp).toBe(undefined);
+    }
+});
